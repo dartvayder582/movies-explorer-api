075d1682d96ce11673742647f522377796a1d3fc
Ivan Akimov <dartvayder582@yandex.ru>
2023-11-13 17:13:19 +0300

    настроить api для запросов с фронта

--- app.js
+++ app.js
@@ -59,5 +59,6 @@ app.use(errors(celebrateConfig));
 app.use(handleErrors);
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -1,7 +1,9 @@
-module.exports = {
+const corsConfig = {
   credentials: true,
   origin: [
     'http://localhost:3001',
     'https://movie.finder.nomoredomainsrocks.ru',
   ],
 };
+
+module.exports = { corsConfig };
